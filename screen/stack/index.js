a91a57bb25ba4c8c0e1de4decbdf070806e2772a
Vachek-hooves <vachek0602@gmail.com>
2025-02-07 17:54:45 +0200

    Restructure screen navigation and add placeholder tab screens

--- screen/stack/index.js
+++ screen/stack/index.js
@@ -1,7 +1,7 @@
-import Activities from './Activities';
-import Game from './Game';
-import Plan from './Plan';
-import Settings from './Settings';
-import Statistic from './Statistic';
+import Activities from '../tab/Activities';
+import Game from '../tab/Game';
+import Plan from '../tab/Plan';
+import Settings from '../tab/Settings';
+import Statistic from '../tab/Statistic';
 
 export {Activities, Game, Plan, Settings, Statistic};
