8cc694773a70e3366a33b3c351c61f01c550a82e
Vachek-hooves <vachek0602@gmail.com>
2025-02-07 17:07:53 +0200

    Add tab navigation screens and icons for Activities, Game, Plan, Settings, and Statistic

---
+++ screen/stack/index.js
@@ -0,0 +1,7 @@
+import Activities from './Activities';
+import Game from './Game';
+import Plan from './Plan';
+import Settings from './Settings';
+import Statistic from './Statistic';
+
+export {Activities, Game, Plan, Settings, Statistic};
---
+++ screen/tabNavigator/TabNav.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Activities, Game, Plan, Settings, Statistic} from '../stack';
+const Tab = createBottomTabNavigator();
+
+const TabNav = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Activities" component={Activities} />
+      <Tab.Screen name="Game" component={Game} />
+      <Tab.Screen name="Plan" component={Plan} />
+      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Statistic" component={Statistic} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNav;
+
+const styles = StyleSheet.create({});
