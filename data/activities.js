2022b0c08996a9e11415f45bc4cd9874fbd9fb5f
Vachek-hooves <vachek0602@gmail.com>
2025-02-07 17:45:10 +0200

    Add activities data with comprehensive descriptions and instructions for various outdoor fitness activities

---
+++ data/activities.js
@@ -0,0 +1,134 @@
+export const activities = [
+  {
+    id: 1,
+    name: 'Running',
+    text: [
+      {
+        type: 'Description',
+        text: '📌 Description:Running is one of the simplest and most accessible forms of exercise, helping you improve cardiovascular health, build endurance, and reduce stress. Whether you’re training for a marathon or simply looking to get outside and stay active, running on different terrains, such as parks or forest trails, can bring new challenges and joys.',
+      },
+      {
+        type: 'Instructions',
+        text: '📖 Instructions:• Warm-up: Begin with light stretching or walking for 5-10 minutes to prepare your muscles.Posture: Keep your body upright, shoulders relaxed, and arms slightly bent at a 90-degree angle.Pacing: Start with a comfortable pace and increase gradually. Avoid rushing. Listen to your body.Breathing: Focus on breathing deeply and rhythmically, in through your nose and out through your mouth.',
+      },
+      {
+        type: 'Tips',
+        text: '🔥 Tips:Warm up and cool down before and after each run to prevent injury and improve flexibility.Try to run on soft surfaces like grass or dirt trails to reduce impact on your joints.If you’re new to running, start with intervals (run-walk) to build stamina.Track your distance and time to see improvements and set goals.Keep your running shoes in good condition to avoid discomfort or injury.',
+      },
+      {
+        type: 'Gear',
+        text: '🎒 Gear:Running shoes with proper support and cushioning.Comfortable moisture-wicking clothing, such as breathable T-shirts and leggings.A sports watch or fitness tracker to monitor your performance.Optional: Sports headphones for music or podcasts during your run.',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Yoga',
+    text: [
+      {
+        type: 'Description',
+        text: 'Yoga is an ancient practice that focuses on strengthening the body, calming the mind, and enhancing flexibility. It’s ideal for improving balance, mental clarity, and reducing stress. Whether you’re doing sun salutations at sunrise or winding down after a long day, yoga on a calm outdoor setting like a park or beach can deepen the experience.',
+      },
+      {
+        type: 'Instructions',
+        text: 'Find a Peaceful Spot: Choose a calm, quiet area with enough space to stretch and move freely.Start with Simple Poses: Begin with basic poses like Downward Dog, Cat-Cow, or Child’s Pose.Breath Control: Practice deep, controlled breathing with each pose. Inhale as you extend or stretch, exhale as you relax.Flow with the Breath: Move smoothly from one pose to another in time with your breath.',
+      },
+      {
+        type: 'Tips',
+        text: '️ Start with a gentle practice before trying more challenging poses.Practice yoga at sunrise or sunset for a more peaceful and rejuvenating experience.Don’t rush – focus on alignment and breathing, rather than on completing a set number of poses.Use props like yoga blocks or straps to support you in difficult poses.Listen to your body – if something feels wrong, stop or modify the pose.',
+      },
+      {
+        type: 'Gear',
+        text: 'A yoga mat for added comfort and grip.Comfortable, stretchy clothes that allow for full movement.Water bottle to stay hydrated.Optional: A meditation cushion for sitting poses and relaxation.',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Hiking',
+    text: [
+      {
+        type: 'Description',
+        text: 'Hiking is a great way to explore nature, get exercise, and enjoy the outdoors. It’s ideal for improving balance, mental clarity, and reducing stress. Whether you’re doing sun salutations at sunrise or winding down after a long day, yoga on a calm outdoor setting like a park or beach can deepen the experience.',
+      },
+      {
+        type: 'Instructions',
+        text: 'Choose a Trail: Select a trail that matches your fitness level and interests.Start with a Warm-Up: Begin with light stretching or walking for 5-10 minutes to prepare your muscles.Posture: Keep your body upright, shoulders relaxed, and arms slightly bent at a 90-degree angle.Pacing: Start with a comfortable pace and increase gradually. Avoid rushing. Listen to your body.Breathing: Focus on breathing deeply and rhythmically, in through your nose and out through your mouth.',
+      },
+      {
+        type: 'Tips',
+        text: 'Start with a gentle practice before trying more challenging poses.Practice yoga at sunrise or sunset for a more peaceful and rejuvenating experience.Don’t rush – focus on alignment and breathing, rather than on completing a set number of poses.Use props like yoga blocks or straps to support you in difficult poses.Listen to your body – if something feels wrong, stop or modify the pose.',
+      },
+      {
+        type: 'Gear',
+        text: 'A yoga mat for added comfort and grip.Comfortable, stretchy clothes that allow for full movement.Water bottle to stay hydrated.Optional: A meditation cushion for sitting poses and relaxation.',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Cycling',
+    text: [
+      {
+        type: 'Description',
+        text: 'Cycling is a great way to get exercise, and enjoy the outdoors. It’s ideal for improving balance, mental clarity, and reducing stress. Whether you’re doing sun salutations at sunrise or winding down after a long day, yoga on a calm outdoor setting like a park or beach can deepen the experience.',
+      },
+      {
+        type: 'Instructions',
+        text: 'Choose a Trail: Select a trail that matches your fitness level and interests.Start with a Warm-Up: Begin with light stretching or walking for 5-10 minutes to prepare your muscles.Posture: Keep your body upright, shoulders relaxed, and arms slightly bent at a 90-degree angle.Pacing: Start with a comfortable pace and increase gradually. Avoid rushing. Listen to your body.Breathing: Focus on breathing deeply and rhythmically, in through your nose and out through your mouth.',
+      },
+      {
+        type: 'Tips',
+        text: 'Start with a gentle practice before trying more challenging poses.Practice yoga at sunrise or sunset for a more peaceful and rejuvenating experience.Don’t rush – focus on alignment and breathing, rather than on completing a set number of poses.Use props like yoga blocks or straps to support you in difficult poses.Listen to your body – if something feels wrong, stop or modify the pose.',
+      },
+      {
+        type: 'Gear',
+        text: 'A yoga mat for added comfort and grip.Comfortable, stretchy clothes that allow for full movement.Water bottle to stay hydrated.Optional: A meditation cushion for sitting poses and relaxation.',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Outdoor Fitness',
+    text: [
+      {
+        type: 'Description',
+        text: 'Outdoor fitness is a great way to get exercise, and enjoy the outdoors. It’s ideal for improving balance, mental clarity, and reducing stress. Whether you’re doing sun salutations at sunrise or winding down after a long day, yoga on a calm outdoor setting like a park or beach can deepen the experience.',
+      },
+      {
+        type: 'Instructions',
+        text: 'Choose a Trail: Select a trail that matches your fitness level and interests.Start with a Warm-Up: Begin with light stretching or walking for 5-10 minutes to prepare your muscles.Posture: Keep your body upright, shoulders relaxed, and arms slightly bent at a 90-degree angle.Pacing: Start with a comfortable pace and increase gradually. Avoid rushing. Listen to your body.Breathing: Focus on breathing deeply and rhythmically, in through your nose and out through your mouth.',
+      },
+      {
+        type: 'Tips',
+        text: 'Start with a gentle practice before trying more challenging poses.Practice yoga at sunrise or sunset for a more peaceful and rejuvenating experience.Don’t rush – focus on alignment and breathing, rather than on completing a set number of poses.Use props like yoga blocks or straps to support you in difficult poses.Listen to your body – if something feels wrong, stop or modify the pose.',
+      },
+      {
+        type: 'Gear',
+        text: 'A yoga mat for added comfort and grip.Comfortable, stretchy clothes that allow for full movement.Water bottle to stay hydrated.Optional: A meditation cushion for sitting poses and relaxation.',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Other Activities',
+    text: [
+      {
+        type: 'Description',
+        text: 'This section covers a variety of other fun and engaging outdoor activities like rollerblading, skateboarding, frisbee, and beach volleyball. These activities are perfect for those who want to try something different or add variety to their outdoor fitness routine.',
+      },
+      {
+        type: 'Instructions',
+        text: ' Choose Your Activity: Pick a sport that excites you, whether it’s skating, playing beach volleyball, or something else.Safety First: Wear the appropriate protective gear, such as pads, helmets, or wrist guards.Warm-up: Always start with a warm-up to prepare your muscles and joints.Techniques: Follow proper techniques to avoid injuries – for example, when rollerblading, keep your knees slightly bent for better control.',
+      },
+      {
+        type: 'Tips',
+        text: '️ Try different activities to find what you enjoy most.If you’re new to the activity, start slow and practice on flat, safe surfaces.Bring friends along for a fun social experience.Know your limits – don’t push too hard if you’re feeling fatigued.',
+      },
+      {
+        type: 'Gear',
+        text: 'Depending on the selected sport (rollerblades, skateboard, etc.).Comfortable athletic wear.A water bottle and snacks.',
+      },
+    ],
+  },
+];
