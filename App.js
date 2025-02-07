9db8fdf619f82e0d5d65cd3ff6d998c5aa7a5e77
Vachek-hooves <vachek0602@gmail.com>
2025-02-07 12:20:39 +0200

    Set up navigation stack with initial screens

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-
+import {Loader, LogIn, Welcome} from './screen/route';
 const Stack = createNativeStackNavigator();
-import { StoreProvider } from './store/context';
+import {StoreProvider} from './store/context';
 
 function App() {
-  return <></>
+  return (
+    <StoreProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Loader" component={Loader} />
+          <Stack.Screen name="LogIn" component={LogIn} />
+          <Stack.Screen name="Welcome" component={Welcome} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StoreProvider>
+  );
 }
 
 export default App;
