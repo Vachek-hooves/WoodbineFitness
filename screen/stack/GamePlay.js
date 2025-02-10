f04990951173418fca00350d26fef3a8f18cfc8f
Vachek-hooves <vachek0602@gmail.com>
2025-02-10 17:12:59 +0200

    Create GamePlay screen as part of game navigation flow

---
+++ screen/stack/GamePlay.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GamePlay = () => {
+  return (
+    <View>
+      <Text>GamePlay</Text>
+    </View>
+  )
+}
+
+export default GamePlay
+
+const styles = StyleSheet.create({})
